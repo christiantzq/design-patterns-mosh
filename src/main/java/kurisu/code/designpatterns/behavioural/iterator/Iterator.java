5208b673bac55cfbb1ebd122ab48ff5b3612116f
Christian Zamora <christiantzq@gmail.com>
2024-03-04 13:26:38 -0500

    iterator review

--- src/main/java/kurisu/code/designpatterns/behavioural/iterator/Iterator.java
+++ src/main/java/kurisu/code/designpatterns/behavioural/iterator/Iterator.java
@@ -1,16 +1,20 @@
-package kurisu.code.designpatterns.behavioural;
+package kurisu.code.designpatterns.behavioural.iterator;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Iterator Design Pattern
+ * 
+ * Creates custom functionality to iterate through a list of objects with the usual
+ * methods like hasNext(), next(), current() using an interface with generics.
+ * 
  * This example illustrates a simple browser history that lets you store
  * and retrieve URLs, but can also provide an Iterator to see the entire
  * list of URLs
  */
 public class Iterator {
-
-    public void test() {
+    public static void main(String[] args) {
         BrowserHistory history = new BrowserHistory();
         history.push("a");
         history.push("b");
@@ -25,6 +29,12 @@ public class Iterator {
     }
 }
 
+/*
+ * A History Class with Iterator capability
+ * 
+ * It uses an embeded concrete implementation then returns it by exposing only the
+ * getIterator()
+ */
 class BrowserHistory {
     private List<String> history = new ArrayList<>();
 
@@ -65,6 +75,9 @@ class BrowserHistory {
 
 }
 
+/*
+ * This is the core
+ */
 interface MyItarable<T> {
     void next();
     T current();
